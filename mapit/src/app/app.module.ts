ac6eae4522cffa97dd3ef20c3c8978b9af7a0b5e
PatrickHickey <patrickstephenhickey@gmail.com>
2017-05-18 21:52:28 -0700

    project8

---
+++ mapit/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AgmCoreModule } from 'angular2-google-maps/core';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBbjY0I2ZHObxShWMUhjG6XBCk90Qcuh9Q'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
